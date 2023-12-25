b27209d4f9b011bb0fe703926ba2156f6561a577
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2023-12-25 17:32:30 +0200

    add code

---
+++ android/app/src/main/kotlin/com/elbashaProg/rohandrayhan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.elbashaProg.rohandrayhan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/rohandrayhan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rohandrayhan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
